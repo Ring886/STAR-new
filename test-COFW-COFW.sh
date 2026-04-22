259b433f2ae240396acec4899ab2f5026b7c6153
ring <2832611491q@gmail.com>
2026-04-22 14:35:46 +0800

    feat: 删除多余test脚本

--- test-COFW-COFW.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python main.py --mode=test --device_ids=0 \
-               --image_dir=image_dir --annot_dir=annot_dir \
-               --data_definition=COFW \
-               --pretrained_weight=model/best_model_COFW.pkl \
-               --ckpt_dir=out_dir
