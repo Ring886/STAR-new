df6d2bf01b1c4f2b25b9f14b44c3db6937a7cb1f
ring <2832611491q@gmail.com>
2026-04-22 14:35:02 +0800

    feat: 修改test脚本

--- test-COFW-COFW.sh
+++ test-COFW-COFW.sh
@@ -2,5 +2,5 @@
 python main.py --mode=test --device_ids=0 \
                --image_dir=image_dir --annot_dir=annot_dir \
                --data_definition=COFW \
-               --pretrained_weight=model/best_model_SEBlock.pkl \
+               --pretrained_weight=model/best_model_COFW.pkl \
                --ckpt_dir=out_dir
