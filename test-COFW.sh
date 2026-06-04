eca5e894c357f8b1ecfd26387583da8226de749f
ring <2832611491q@gmail.com>
2026-06-04 16:22:56 +0800

    feat: 更换模型名

--- test-COFW.sh
+++ test-COFW.sh
@@ -1,5 +1,5 @@
 python main.py --mode=test --device_ids=0 \
                --image_dir=image_dir --annot_dir=annot_dir \
                --data_definition=COFW \
-               --pretrained_weight=model/COFW_STARLoss_NME_4_62.pkl \
+               --pretrained_weight=model/100epochs_starloss_nocoordconv.pkl \
                --ckpt_dir=out_dir
