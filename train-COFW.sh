8e5225486f80c42bad961f7930d77747180e5ba1
ring <2832611491q@gmail.com>
2026-06-04 15:01:38 +0800

    chore: device_id

--- train-COFW.sh
+++ train-COFW.sh
@@ -1,4 +1,4 @@
-python main.py --mode=train --device_ids=0,1,2,3 \
+python main.py --mode=train --device_ids=0 \
                --image_dir=image_dir --annot_dir=annot_dir \
                --data_definition=COFW \
                --ckpt_dir=out_dir
